91ee095a093208476c0b7aef994664adb11ddcc2
Andy5am <38666859+Andy5am@users.noreply.github.com>
2021-06-03 18:49:21 -0600

    Inicio de nuevo

--- hoppers.py
+++ hoppers.py
@@ -1,336 +1,61 @@
-#Andy Castillo 18040
-#IA Hoppers
+import sys
+import time
+import math
+
+from board import Board, Tile
 
 
-victory = 0
-coords=['0','1','2','3','4','5','6','7','8','9']
-board=[
-    ['X','X','X','X','X','-','-','-','-','-'],
-    ['X','X','X','X','-','-','-','-','-','-'],
-    ['X','X','X','-','-','-','-','-','-','-'],
-    ['X','X','-','-','-','-','-','-','-','-'],
-    ['X','-','-','-','-','-','-','-','-','-'],
-    ['-','-','-','-','-','-','-','-','-','O'],
-    ['-','-','-','-','-','-','-','-','O','O'],
-    ['-','-','-','-','-','-','-','O','O','O'],
-    ['-','-','-','-','-','-','O','O','O','O'],
-    ['-','-','-','-','-','O','O','O','O','O'],
-]
+class Hoppers():
 
-def print_board(board):
-    string='  '
-    for coord in coords:
-        string+=coord+' '
-    print(string+'\n')
-    counter = 0
-    for line in board:
-        string=''
-        for cell in line:
-            string+=cell+' '
-        print(str(counter)+' '+string)
-        counter+=1
+    def __init__(self, b_size=8, t_limit=60, c_player=Tile.P_RED):
 
-def turn(board, turn):
-    if(turn):
-        player_char="X"
-    else:
-        player_char="O"
-    empty_char="-"
-    if(not turn):
-        initial_board = board
-        temp_board = board
-    # Jugadas se ingresa Y-X,Y-X,...
-        play = input("Ingrese su jugada: ")
-        #separar los saltos
-        play = play.replace(" ","")
-        jump_list = play.split(',')
-        print(jump_list)
+        # Se crea el tablero
+        board = [[None] * b_size for _ in range(b_size)]
+        for row in range(b_size):
+            for col in range(b_size):
 
-        #Quitar la posicion inicial de la lista
-        start_pos=jump_list.pop(0)
-        start_X = int(start_pos.split("-")[0])
-        start_Y = int(start_pos.split("-")[1])
+                if row + col < 4:
+                    element = Tile(2, 2, 0, row, col)
+                elif row + col > 2 * (b_size - 3):
+                    element = Tile(1, 1, 0, row, col)
+                else:
+                    element = Tile(0, 0, 0, row, col)
 
-        #separar coordenadas de cada salto
-        for jump in  jump_list:
-            jump_coordY = int(jump.split("-")[1])
-            jump_coordX = int(jump.split("-")[0])
+                board[row][col] = element
 
-            valid = validate_jump(start_X,start_Y,jump_coordX,jump_coordY,board)
-            if (valid):
-                temp_board[start_X][start_Y] = empty_char
-                temp_board[jump_coordX][jump_coordY] = player_char
+        # Variables
+        self.b_size = b_size
+        self.t_limit = t_limit
+        self.c_player = c_player
+        self.board_view = Board(board)
+        self.board = board
+        self.current_player = Tile.P_BLACK
+        self.selected_tile = None
+        self.valid_moves = []
+        self.computing = False
+        self.total_plies = 0
 
-                start_X = jump_coordX
-                start_Y = jump_coordY
-            else:
-                print("Jugada invalida")
-                board=initial_board
-                return(False)
-        
-        board=temp_board
-    else:
-        move = minimax(board, 1, turn)[1]
-        start_pos = move[0]
-        final_pos = move[1]
-        board[int(start_pos.split("-")[0])][int(start_pos.split("-")[1])] = empty_char
-        board[int(final_pos.split("-")[0])][int(final_pos.split("-")[1])] = player_char
-        print(move)
-        return(True)
+        self.ply_depth = 3
+        self.ab_enabled = True
 
-    return(True)
+        self.r_goals = [t for row in board
+                        for t in row if t.tile == Tile.T_RED]
+        self.g_goals = [t for row in board
+                        for t in row if t.tile == Tile.T_BLACK]
+
+        self.board_view.set_status_color("#E50000" if
+            self.current_player == Tile.P_RED else "#007F00")
+
+        if self.c_player == self.current_player:
+            self.AI_Move()
+
+        self.board_view.add_click_handler(self.tile_clicked)
+        self.board_view.draw_tiles(board=self.board)  # Se recarga el tablero
+
+        self.board_view.mainloop()  # Tkinter loop
 
 
-def validate_jump(initX, initY, finalX, finalY, board):
-    valid = False
 
-    if(player_turn):
-        player_char="X"
-    else:
-        player_char="O"
-    empty_char="-"
+if __name__ == "__main__":
 
-    #Verificar que haya pieza ahi
-    init_cell = board[initX][initY]
-    final_cell = board[finalX][finalY]
-
-    if(init_cell==player_char and final_cell==empty_char):
-        #validar el salto
-        difX = initX-finalX
-        difY = initY-finalY
-        
-        #Cuando es mover al lado
-        if((difX==0 and difY==1) or (difX==-1 and difY==1) or (difX==1 and difY==1) or (difX==1 and difY==0) or (difX==-1 and difY==0) or (difX==0 and difY==-1) or (difX==1 and difY==-1) or (difX==-1 and difY==-1)):
-            valid=True
-            return(valid)
-        elif((difX==0 and difY==2) or (difX==-2 and difY==2) or (difX==2 and difY==2) or (difX==2 and difY==0) or (difX==-2 and difY==0) or (difX==0 and difY==-2) or (difX==2 and difY==-2) or (difX==-2 and difY==-2)):
-            #cuando salta una ficha
-            if(difX==2):
-                difX = 1
-            elif(difX==-2):
-                difX = -1
-            if(difY==2):
-                difY = 1
-            elif(difY==-2):
-                difY = -1
-            
-            #Verificar que la de en medio este ocupada
-            middleX = finalX+difX
-            middleY = finalY+difY
-            middle_Cell = board[middleX][middleY]
-            if(middle_Cell!=empty_char):
-                valid = True
-                return(valid)
-            else:
-                return(valid)
-        else:
-            return(valid)
-    else:
-        #no es valida la jugada
-        return(valid)
-
-def validate_win(board):
-    x_counter_top = 0
-    o_counter_top = 0
-    x_counter_bottom = 0
-    o_counter_bottom = 0
-    for i in range(10):
-        if (i<5):
-            for j in range(5-i):
-                if(board[i][j]=="X"):
-                    x_counter_top+=1
-                elif(board[i][j]=="O"):
-                    o_counter_top+=1
-        if (i>4):
-            for k in range(14-i,10):
-                if(board[i][k]=="X"):
-                    x_counter_bottom+=1
-                elif(board[i][k]=="O"):
-                    o_counter_bottom+=1
-    if(x_counter_top+o_counter_top==15 and x_counter_top<15):
-        return(1)
-    elif(x_counter_bottom+o_counter_bottom==15 and o_counter_bottom<15):
-        return(-1)
-    else:
-        return(0)
-
-def get_possible_moves(board, turn):
-    if(turn):
-        player_char="X"
-    else:
-        player_char="O"
-    moves=[]
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if (board[i][j]==player_char):
-                coords = get_around_coords(i, j)
-                for coord in coords:
-                    if(validate_jump(i,j,int(coord.split("-")[0]),int(coord.split("-")[1]),board)):
-                        move=[]
-                        move.append(str(i)+'-'+str(j))
-                        move.append(coord)
-                        moves.append(move)
-    return moves
-
-
-def get_around_coords(x, y):
-    if(x==0):
-        if(y==0):
-            return([str(x+1)+'-'+str(y), str(x)+'-'+str(y+1), str(x+1)+'-'+str(y+1), str(x+2)+'-'+str(y), str(x)+'-'+str(y+2), str(x+2)+'-'+str(y+2)])
-        elif(y==9):
-            return([str(x+1)+'-'+str(y), str(x)+'-'+str(y-1), str(x+1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x)+'-'+str(y-2), str(x+2)+'-'+str(y-2)])
-        elif(y==1):
-            return([str(x+1)+'-'+str(y), str(x)+'-'+str(y-1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x+1)+'-'+str(y+1), str(x+2)+'-'+str(y), str(x)+'-'+str(y+2), str(x+2)+'-'+str(y+2)])
-        elif(y==8):
-            return([str(x+1)+'-'+str(y), str(x)+'-'+str(y-1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x+1)+'-'+str(y+1), str(x+2)+'-'+str(y), str(x)+'-'+str(y-2), str(x+2)+'-'+str(y-2)])
-        else:
-            return([str(x+1)+'-'+str(y), str(x)+'-'+str(y-1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x+1)+'-'+str(y+1), str(x+2)+'-'+str(y), str(x)+'-'+str(y-2), str(x+2)+'-'+str(y-2), str(x)+'-'+str(y+2), str(x+2)+'-'+str(y+2)])
-    elif(x==9):
-        if(y==0):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y+1), str(x-2)+'-'+str(y), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y+2)])
-        elif(y==9):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y-1), str(x-1)+'-'+str(y-1), str(x-2)+'-'+str(y), str(x)+'-'+str(y-2), str(x-2)+'-'+str(y-2)])
-        elif(y==8):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y-1), str(x-1)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y+1), str(x-2)+'-'+str(y), str(x)+'-'+str(y-2), str(x-2)+'-'+str(y-2)])
-        elif(y==1):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y-1), str(x-1)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y+1), str(x-2)+'-'+str(y), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y+2)])
-        else:
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y-1), str(x-1)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y+1), str(x-2)+'-'+str(y), str(x)+'-'+str(y-2), str(x-2)+'-'+str(y-2), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y+2)])
-    elif(x==1):
-        if(y==1):
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y+2), str(x)+'-'+str(y+2)])
-        elif(y==8):
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y-2), str(x)+'-'+str(y-2)])
-        elif(y==0):
-            return([str(x+1)+'-'+str(y), str(x)+'-'+str(y+1), str(x+1)+'-'+str(y+1), str(x+2)+'-'+str(y), str(x)+'-'+str(y+2), str(x+2)+'-'+str(y+2)])
-        elif(y==9):
-            return([str(x+1)+'-'+str(y), str(x)+'-'+str(y-1), str(x+1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x)+'-'+str(y-2), str(x+2)+'-'+str(y-2)])
-        else:
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y+2), str(x+2)+'-'+str(y-2), str(x)+'-'+str(y-2), str(x)+'-'+str(y+2)])
-    elif(x==8):
-        if(y==1):
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y), str(x-2)+'-'+str(y+2)])
-        elif(y==8):
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x)+'-'+str(y-2), str(x-2)+'-'+str(y), str(x-2)+'-'+str(y-2)])
-        elif(y==0):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y+1), str(x-2)+'-'+str(y), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y+2)])
-        elif(y==9):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y-1), str(x-1)+'-'+str(y-1), str(x-2)+'-'+str(y), str(x)+'-'+str(y-2), str(x-2)+'-'+str(y-2)])
-        else:
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x)+'-'+str(y-2), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y), str(x-2)+'-'+str(y+2), str(x-2)+'-'+str(y-2)])
-    else:
-        if(y==0):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y+1), str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x-2)+'-'+str(y), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y+2), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y+2)])
-        elif(y==9):
-            return([str(x-1)+'-'+str(y), str(x)+'-'+str(y-1), str(x-1)+'-'+str(y-1), str(x+1)+'-'+str(y), str(x+1)+'-'+str(y-1), str(x-2)+'-'+str(y), str(x)+'-'+str(y-2), str(x-2)+'-'+str(y-2), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y-2)])
-        elif(y==1):
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y+2), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y), str(x-2)+'-'+str(y+2)])
-        elif(y==8):
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y-2), str(x)+'-'+str(y-2), str(x-2)+'-'+str(y), str(x-2)+'-'+str(y-2)])
-        else:
-            return([str(x+1)+'-'+str(y), str(x+1)+'-'+str(y+1), str(x+1)+'-'+str(y-1), str(x)+'-'+str(y-1), str(x)+'-'+str(y+1), str(x-1)+'-'+str(y), str(x-1)+'-'+str(y+1), str(x-1)+'-'+str(y-1), str(x+2)+'-'+str(y), str(x+2)+'-'+str(y+2), str(x+2)+'-'+str(y-2), str(x)+'-'+str(y-2), str(x)+'-'+str(y+2), str(x-2)+'-'+str(y), str(x-2)+'-'+str(y+2), str(x-2)+'-'+str(y-2)])
-
-def utility(board, turn):
-    x_counter_top = 0
-    o_counter_top = 0
-    x_counter_bottom = 0
-    o_counter_bottom = 0
-    up_left_counter = 0
-    down_rigth_counter = 0
-    for i in range(10):
-        if (i<5):
-            for j in range(5-i):
-                if(board[i][j]=="X"):
-                    x_counter_top+=1
-                elif(board[i][j]=="O"):
-                    o_counter_top+=1
-        if (i>4):
-            for k in range(14-i,10):
-                if(board[i][k]=="X"):
-                    x_counter_bottom+=1
-                elif(board[i][k]=="O"):
-                    o_counter_bottom+=1
-    for i in range(10):
-        if (i<5):
-            for j in range(5-i,10):
-                if(board[i][j]=="X"):
-                    down_rigth_counter+= (9-i+9-j)
-                if(board[i][j]=="O"):
-                    up_left_counter+= (i+j)
-        if (i>4):
-            for k in range(0,14-i):
-                if(board[i][k]=="X"):
-                    down_rigth_counter+= (9-i+9-k)
-                if(board[i][k]=="O"):
-                    up_left_counter+= (i+k)
-                    
-    return(x_counter_bottom**2 + o_counter_bottom**2 - x_counter_top**2 - o_counter_top**2 + up_left_counter - down_rigth_counter)
-
-    
-def minimax(board, depth, turn, alpha=float('-inf'), beta=float('inf')):
-    best_move = None
-
-    if (validate_win(board) or depth == 0):
-        return [utility(board, turn), None]
-
-    if (turn):
-        best_value = float('-inf')
-        other_player = not turn
-    else:
-        best_value = float('inf')
-        other_player = turn
-
-    if (turn):
-        player = turn
-    else:
-        player = other_player
-
-    moves = get_possible_moves(board, player)
-
-
-    for move in moves :
-        
-        start_pos = move[0]
-        final_pos = move[1]
-
-        player_char = board[int(start_pos.split("-")[0])][int(start_pos.split("-")[1])]
-
-        board[int(start_pos.split("-")[0])][int(start_pos.split("-")[1])] = "-"
-        board[int(final_pos.split("-")[0])][int(final_pos.split("-")[1])] = player_char
-
-        value, movement = minimax(board, depth -1, other_player, alpha, beta)
-
-        board[int(start_pos.split("-")[0])][int(start_pos.split("-")[1])] = player_char
-        board[int(final_pos.split("-")[0])][int(final_pos.split("-")[1])] = "-"
-
-        if (turn):
-            if value > best_value:
-                best_move = move
-                best_value = value
-                alpha = max(alpha, best_value)
-        else:
-            if value < best_value:
-                best_move = move
-                best_value = value
-                beta = min(beta, best_value)
-
-        if (turn and alpha > beta) or (not turn and alpha < beta):
-            return [best_value, best_move]
-
-    return [best_value, best_move]
-
-
-player_turn = True
-print_board(board)
-while(victory==0):
-    if player_turn:
-        print("Turno IA: ")
-    else:
-        print("Turno jugador 2: ")
-    played = turn(board,player_turn)
-    if (played):
-        player_turn = not player_turn
-        print_board(board)
-        victory = validate_win(board)
-
-print(victory)
+    hoppers = Hoppers()
